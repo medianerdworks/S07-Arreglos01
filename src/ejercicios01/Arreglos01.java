aa2d52da64e8c9c480119cf89da3c897db6ae9ec
macbookpro <rmedinap@gmail.com>
2017-05-17 09:40:27 -0500

    Iniciando proyecto java con metodo Principal y metodo Arreglos01.

---
+++ src/ejercicios01/Arreglos01.java
@@ -0,0 +1,13 @@
+package ejercicios01;
+
+public class Arreglos01 {
+
+    void iniciar() {
+        arreglo01();
+    }
+
+    private void arreglo01() {
+        
+    }
+    
+}
---
+++ src/ejercicios01/Principal.java
@@ -0,0 +1,8 @@
+package ejercicios01;
+
+public class Principal {
+    public static void main(String[] args) {
+        Arreglos01 arrg= new Arreglos01();
+        arrg.iniciar();
+    }
+}
